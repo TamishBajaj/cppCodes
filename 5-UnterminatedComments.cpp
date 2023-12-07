4335207a0f8423c6be2b7324c5139c7728cb50dc
Tamish <tamishbajaj360@gmail.com>
2023-12-08 01:00:22 +0530

    small change

--- 5-UnterminatedComments.cpp
+++ 5-UnterminatedComments.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+using namespace std;
 
 bool hasUnterminatedComment(const char* code) {
     bool inComment = false;
@@ -22,11 +23,11 @@ int main() {
     const int maxSize = 1000;
     char code[maxSize] = {0}; // Initialize the array to avoid undefined behavior
 
-    std::cout << "Enter your C++ code (terminate input with a period '.' on a line by itself):\n";
+    cout << "Enter your C++ code (terminate input with a period '.' on a line by itself):\n";
 
     while (true) {
         char line[100];
-        std::cin.getline(line, sizeof(line));
+        cin.getline(line, sizeof(line));
 
         if (line[0] == '.' && line[1] == '\0') {
             break;
@@ -36,9 +37,9 @@ int main() {
     }
 
     if (hasUnterminatedComment(code)) {
-        std::cout << "Error: Unterminated multi-line comment found.\n";
+        cout << "Error: Unterminated multi-line comment found.\n";
     } else {
-        std::cout << "No unterminated multi-line comments found.\n";
+        cout << "No unterminated multi-line comments found.\n";
     }
 
     return 0;
